c76a1b11ec02946539f9acca3bba26f869791dd0
Osma Suominen <osma.suominen@helsinki.fi>
2018-10-24 15:33:13 +0300

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='finna-client',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/NatLibFi/Finna-client',
     author='Osma Suominen',
     author_email='osma.suominen@helsinki.fi',
